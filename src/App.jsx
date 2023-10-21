8b885d9376ee3d411000b190399c5a06dc0fc86a
VikashMaurya10 <VikashMaurya10stp@gmail.com>
2023-10-21 09:39:29 +0530

    add scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import PageNotFound from './pages/PageNotFound'
 import ComingSoon from './pages/ComingSoon'
 import PrivacyPolicy from './pages/PrivacyPolicy'
 
 const App = () => {
+
+  const ScrollToTop = () => {
+    const location = useLocation();
+
+    useEffect(() => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    }, [location])
+  }
+
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/home' element={<Home />} />
-      <Route path='/privacy-policy' element={<PrivacyPolicy />} />
-      <Route path='/comming-soon' element={<ComingSoon />} />
-      <Route path="*" element={<PageNotFound />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/privacy-policy' element={<PrivacyPolicy />} />
+        <Route path='/comming-soon' element={<ComingSoon />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </>
   )
 }
 
